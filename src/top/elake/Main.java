97b4a68567e93044cab5531e96cf0872e7fa0408
Erhai_lake <fuzixuan0714_0826@163.com>
2024-06-07 15:12:27 +0800

    又勉强写了个挂号管理

--- src/top/elake/Main.java
+++ src/top/elake/Main.java
@@ -2,8 +2,7 @@ package top.elake;
 
 public class Main {
   public static void main(String[] args) {
-    System.out.println("*** XXX医院管理系统 ***");
     Menu Menu = new Menu();
-    Menu.Main1();
+    Menu.MainMenu();
   }
 }
--- src/top/elake/Menu.java
+++ src/top/elake/Menu.java
@@ -3,8 +3,10 @@ package top.elake;
 import java.util.Scanner;
 
 public class Menu {
-  public void Main1() {
-    java.util.Scanner Scanner = new Scanner(System.in);
+  java.util.Scanner Scanner = new Scanner(System.in);
+
+  public void MainMenu() {
+    System.out.println("*** XXX医院管理系统 ***");
     System.out.println("1. 挂号管理");
     System.out.println("2. 收费管理");
     System.out.println("3. 药品管理");
@@ -14,6 +16,7 @@ public class Menu {
     int Input = Scanner.nextInt();
     switch (Input) {
       case 1:
+        RegistrationMenu();
         break;
       case 2:
         break;
@@ -23,7 +26,27 @@ public class Menu {
         break;
       default:
         System.out.println("输入有误,请重新输入");
+        MainMenu();
     }
-    System.out.println(Input);
+  }
+
+  public void RegistrationMenu() {
+    System.out.println("*** 挂号管理 ***");
+    System.out.println("1. 新增");
+    System.out.println("2. 删除");
+    System.out.println("3. 列出");
+    int Input = Scanner.nextInt();
+    switch (Input) {
+      case 1:
+        RegistrationMenu();
+        break;
+      case 2:
+        break;
+      case 3:
+        break;
+      default:
+        System.out.println("输入有误,请重新输入");
+        RegistrationMenu();
+    }
   }
 }

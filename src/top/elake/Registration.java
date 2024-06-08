4ecf20b60d6a2ca679ecfb481441028768f82f2e
Erhai_lake <fuzixuan0714_0826@163.com>
2024-06-08 16:48:01 +0800

    完善了挂号的新建,选中,删除,同时支持使用名称或者电话号码查找

--- src/top/elake/Registration.java
+++ src/top/elake/Registration.java
@@ -18,42 +18,43 @@ public class Registration {
   // 菜单
   public void Menu() {
     boolean Status = true;
+    String UserName;
+    String Cell;
+    int SectionId;
     do {
       System.out.println("*** 挂号管理 ***");
       System.out.println("1. 新增");
-      System.out.println("2. 删除");
-      System.out.println("3. 查询");
-      System.out.println("4. 返回");
+      System.out.println("2. 查询");
+      System.out.println("3. 返回");
       System.out.print("请输入对应的编号: ");
       int Input = Scanner.nextInt();
       switch (Input) {
         case 1:
           // 新增
+          System.out.print("请输入用户名: ");
+          UserName = Scanner.next();
+          System.out.print("请输入电话号码: ");
+          Cell = Scanner.next();
+          if (UserName.isEmpty() || Cell.isEmpty()) {
+            System.out.println("用户名或电话号码不能为空");
+            break;
+          }
+          // 等科室写完,这里写一个选择科室的
+          SectionId = 1;
+          // 暂时使用1
+          New(UserName, Cell, SectionId);
           break;
         case 2:
-          // 删除
+          // 查询
+          System.out.print("请输入你要查询的名称或电话号码: ");
+          UserName = Scanner.next();
+          if (UserName.isEmpty()) {
+            System.out.println("名称或电话号码不能为空");
+            break;
+          }
+          Query(UserName, !UserName.matches("[0-9]+"));
           break;
         case 3:
-          // 查询
-          System.out.print("请输入你要查询的用户名: ");
-          String UserName = Scanner.next();
-          try {
-            String SQL = "SELECT * FROM Registration WHERE UserName = ?";
-            PreparedStatement PreparedStatement = MySQLConnection.prepareStatement(SQL);
-            PreparedStatement.setString(1, UserName);
-            ResultSet ResultSet = PreparedStatement.executeQuery();
-            if (!ResultSet.next()) {
-              System.out.println("没有找到匹配的数据");
-            } else {
-              do {
-                System.out.println("ID: " + ResultSet.getInt("RegistrationId") + ", Name: " + ResultSet.getString("UserName"));
-              } while (ResultSet.next());
-            }
-          } catch (SQLException e) {
-            System.out.println("查询数据时出现错误:" + e.getMessage());
-          }
-          break;
-        case 4:
           // 返回
           Status = false;
           break;
@@ -62,4 +63,93 @@ public class Registration {
       }
     } while (Status);
   }
+
+  // 选中菜单
+  public void SelectedMenu() {
+    System.out.print("请选择要操作的编号: ");
+    int ID = Scanner.nextInt();
+    boolean Status = true;
+    do {
+      System.out.println("*** 操作 ***");
+      System.out.println("1. 删除");
+      System.out.println("2. 重新选择");
+      System.out.println("3. 返回");
+      System.out.print("请输入对应的编号: ");
+      int Input = Scanner.nextInt();
+      switch (Input) {
+        case 1:
+          // 删除
+          Status = Delete(ID);
+          break;
+        case 2:
+          // 重新选择
+          System.out.print("请选择要操作的编号: ");
+          ID = Scanner.nextInt();
+          break;
+        case 3:
+          // 返回
+          Status = false;
+          break;
+        default:
+          System.out.println("输入有误,请重新输入");
+      }
+    } while (Status);
+  }
+
+  // 新增
+  public void New(String UserName, String Cell, int SectionId) {
+    try {
+      String SQL = "INSERT INTO Registration (UserName, Cell, SectionId) VALUES (?, ?, ?)";
+      PreparedStatement PreparedStatement = MySQLConnection.prepareStatement(SQL);
+      PreparedStatement.setString(1, UserName);
+      PreparedStatement.setString(2, Cell);
+      PreparedStatement.setInt(3, SectionId);
+      PreparedStatement.executeUpdate();
+      System.out.println("挂号成功");
+    } catch (SQLException e) {
+      System.out.println("挂号时出现错误:" + e.getMessage());
+    }
+  }
+
+  // 查询
+  public void Query(String Value, Boolean Type) {
+    String SQL;
+    if (Type) {
+      SQL = "SELECT * FROM Registration WHERE UserName = ?";
+    } else {
+      SQL = "SELECT * FROM Registration WHERE Cell = ?";
+    }
+    try {
+      PreparedStatement PreparedStatement = MySQLConnection.prepareStatement(SQL);
+      PreparedStatement.setString(1, Value);
+      ResultSet ResultSet = PreparedStatement.executeQuery();
+      if (!ResultSet.next()) {
+        System.out.println("没有找到匹配的数据");
+      } else {
+        System.out.println("编号\t名称\t电话号码\t科室");
+        do {
+          System.out.println(ResultSet.getInt("RegistrationId") + "\t" + ResultSet.getString("UserName") + "\t" + ResultSet.getString("Cell") + "\t" + ResultSet.getString("SectionId"));
+        } while (ResultSet.next());
+        SelectedMenu();
+      }
+    } catch (SQLException e) {
+      System.out.println("查询挂号时出现错误:" + e.getMessage());
+    }
+  }
+
+  // 删除
+  public Boolean Delete(int RegistrationId) {
+    try {
+      String SQL = "DELETE FROM Registration WHERE RegistrationId = ?";
+      PreparedStatement PreparedStatement = MySQLConnection.prepareStatement(SQL);
+      PreparedStatement.setInt(1, RegistrationId);
+      PreparedStatement.executeUpdate();
+      System.out.println("挂号删除成功");
+      return false;
+    } catch (SQLException e) {
+      System.out.println("删除挂号时出现错误:" + e.getMessage());
+      return true;
+    }
+  }
+
 }
